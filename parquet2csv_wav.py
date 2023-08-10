375bd1116f3d47b8b06c3f632c45e92a70a6317a
mirix <miromoman@gmail.com>
2023-08-10 17:44:51 +0200

    Add files via upload

--- parquet2csv_wav.py
+++ parquet2csv_wav.py
@@ -16,6 +16,10 @@ n_cores = str(os.cpu_count())
 os.environ['OMP_NUM_THREADS'] = n_cores
 os.environ['MKL_NUM_THREADS'] = n_cores
 
+# Define directory to store the samples
+cwd = os.getcwd()
+sample_dir = str(cwd) + '/wavs/'
+
 # Create the wavs dir if it does not exist
 if not os.path.isdir('wavs'):
 	os.makedirs('wavs')
@@ -23,12 +27,12 @@ if not os.path.isdir('wavs'):
 # All columns from the parquet file except the one with the audio numpy arrays (it is huge)
 columns = ['ytid', 'ytid_seg', 'start', 'end', 'sentiment', 'happiness', 'sadness', 'anger', 'fear', 'disgust', 'surprise']
 
-### Add replace line here to change the paths from the ytid_seg column ###
-# https://pola-rs.github.io/polars/py-polars/html/reference/expressions/api/polars.Expr.str.replace.html
-# replace the subtring '/home/emoman/Downloads/mosei/samples' with the actual path
-
 # Read the parquet file with polars
 df = pl.read_parquet('sqe_messai.parquet', columns = columns)
+
+# Replace the generic path with the actual path
+bad_dir = df.row(0)[1].rsplit('/', 1)[0] + '/'
+df = df.with_columns(pl.col('ytid_seg').str.replace_all(bad_dir, sample_dir))
 
 # Export the csv file (excluding the last column)
 df.write_csv('sqe_messai_nowav.csv')
@@ -40,15 +44,15 @@ columns2 = ['ytid_seg', 'wav2numpy']
 # Read the parquet file with polars (this will take a while)
 df2 = pl.read_parquet('sqe_messai.parquet', use_pyarrow=False, columns = columns2)
 
-### Add replace line here to change the paths from the ytid_seg column ###
-# https://pola-rs.github.io/polars/py-polars/html/reference/expressions/api/polars.Expr.str.replace.html
-# replace the subtring '/home/emoman/Downloads/mosei/samples' with the actual path
+# Replace the generic path with the actual path
+bad_dir = df2.row(0)[0].rsplit('/', 1)[0] + '/'
+df2 = df2.with_columns(pl.col('ytid_seg').str.replace_all(bad_dir, sample_dir))
 
 # Function to convert the numpy arrays to wav files stored in the wavs folders
 def numpy2wav(row):
 	segment = os.path.splitext(os.path.basename(os.path.normpath(row[0])))[0]
 	print('PROCESSED:', segment)
-	write('wavs/' + segment + '.wav', 16000, np.array(row[1]))
+	write(sample_dir + segment + '.wav', 16000, np.array(row[1]))
 	return segment
 
 # Apply the function (this will take a while)
